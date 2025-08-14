7c650e7daf215e8e922355157cc4952813b3aff0
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2025-08-14 18:33:30 +0000

    Refactor main function to initialize clock and frame rate control

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids")
-    
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -14,6 +16,9 @@ def main():
     
         screen.fill(pygame.Color("black"))
         pygame.display.flip()
+
+        # Limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000.0 
 
 if __name__ == "__main__":
     main()
