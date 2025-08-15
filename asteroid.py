84640915d180e2e990d9cfa604860a16fdaac138
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2025-08-15 20:46:50 +0000

    Implement asteroid splitting functionality

--- asteroid.py
+++ asteroid.py
@@ -1,10 +1,31 @@
 import pygame
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float):
         super().__init__(x, y, radius)
+
+    def split(self) -> None:
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.circle(
