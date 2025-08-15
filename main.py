6c9bae53bfc4a4a4a88115bdfc162db37b7ab537
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2025-08-15 17:56:02 +0000

    Refactor main function to initialize updatable and drawable groups before player creation

--- main.py
+++ main.py
@@ -7,19 +7,27 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
-    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable: pygame.sprite.Group[pygame.sprite.Sprite] = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0.0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
+        updatable.update(dt)
+
         screen.fill(pygame.Color("black"))
-        
-        player.update(dt)
-        player.draw(screen)
+
+        for entity in drawable:
+            entity.draw(screen)
 
         pygame.display.flip()
 
